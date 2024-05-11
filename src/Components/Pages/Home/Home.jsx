e1459db21a6760f633b4eac7c09e4eb22360c577
jahid05 <jahidcse05@gmail.com>
2024-05-11 21:23:19 +0600

    Preloading implement

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -4,17 +4,16 @@ import PopularPackages from "../PopularPackages/PopularPackages";
 import Work from "../Work/Work";
 import HeroSection from "./HeroSection/HeroSection";
 
-
 const Home = () => {
-    return (
-        <div className="font-customFont">
-            <HeroSection></HeroSection>
-            <Work></Work>
-            <About></About>
-            <PopularPackages></PopularPackages>
-            <GlobalPromotion></GlobalPromotion>
-        </div>
-    );
+  return (
+    <div className="font-customFont">
+      <HeroSection></HeroSection>
+      <Work></Work>
+      <About></About>
+      <PopularPackages></PopularPackages>
+      <GlobalPromotion></GlobalPromotion>
+    </div>
+  );
 };
 
 export default Home;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,31 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../Components/Shared/Footer/Footer';
 import Nav from '../Components/Shared/Nav/Nav';
+import { useEffect, useState } from "react";
+import { ScaleLoader } from "react-spinners";
 
 const Routes = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 500)
+    }, [])
     return (
         <div className="font-customFont">
-            <Nav></Nav>
+            {
+                loading? <div className="flex justify-center items-center h-screen">
+                    <ScaleLoader color="#00E5CC" />
+                </div> :
+                
+                <>
+                    <Nav></Nav>
             <Outlet></Outlet>
             <Footer></Footer>
+                </>
+            }
         </div>
     );
 };
